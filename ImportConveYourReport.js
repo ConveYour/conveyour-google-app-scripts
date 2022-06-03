65e47fbffcaccc048a8bf2a34ebe1b449784b267
Stephen Rhyne <stephen@throwingboulders.com>
2022-06-03 13:47:51 -0500

    Google Sheet function to import live data feed of report from ConveYour

---
+++ ImportConveYourReport.js
@@ -0,0 +1,32 @@
+function ImportConveYourReport(url) {
+  let json = UrlFetchApp.fetch(url);
+  let res   = JSON.parse(json.getContentText());
+  if( !res || res.status !== 'ok' || !res.data ){
+    return `failed: ${res.message}`;
+  }
+  
+  let data = res.data;
+  let headers = [];
+
+  let dataRows = data.data || [];
+  let sheetRows = [];
+  let addSheetRow = row => {
+    const sheetRow = headers.map( header => row[header] || null )
+    console.log(sheetRow)
+    sheetRows.push(sheetRow)
+  };
+
+  if(data.fields){
+    headers = Object.keys(data.fields);
+  }
+  else{
+    headers = Object.keys(dataRows[0] || []);
+  }
+  
+  dataRows.forEach(addSheetRow)
+
+  sheetRows.unshift(headers)
+
+  return sheetRows
+
+}
