e0255accb7f6660b112ca1f0bc74892ea5d9648f
Stephen Rhyne <stephen@throwingboulders.com>
2022-06-03 13:50:14 -0500

    Fixes 0 values returning as null instead of 0

--- ImportConveYourReport.js
+++ ImportConveYourReport.js
@@ -1,28 +1,32 @@
 function ImportConveYourReport(url) {
   let json = UrlFetchApp.fetch(url);
-  let res   = JSON.parse(json.getContentText());
-  if( !res || res.status !== 'ok' || !res.data ){
+  let res = JSON.parse(json.getContentText());
+  if (!res || res.status !== 'ok' || !res.data) {
     return `failed: ${res.message}`;
   }
-  
+
   let data = res.data;
   let headers = [];
 
   let dataRows = data.data || [];
   let sheetRows = [];
   let addSheetRow = row => {
-    const sheetRow = headers.map( header => row[header] || null )
+    const sheetRow = headers.map(header => {
+      let value = row[header];
+      return value === undefined ? null : value;
+    })
     console.log(sheetRow)
     sheetRows.push(sheetRow)
   };
 
-  if(data.fields){
+
+  if (data.fields) {
     headers = Object.keys(data.fields);
   }
-  else{
+  else {
     headers = Object.keys(dataRows[0] || []);
   }
-  
+
   dataRows.forEach(addSheetRow)
 
   sheetRows.unshift(headers)
